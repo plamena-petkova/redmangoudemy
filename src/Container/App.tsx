bb829e0aaa1457b856faac63ed303e34f35b670e
Plamena Petkova <pl.petkova@gmail.com>
2025-01-13 10:11:39 +0200

    Initial commit

---
+++ src/Container/App.tsx
@@ -0,0 +1,15 @@
+import "../index.css";
+import "bootstrap/dist/css/bootstrap.min.css"
+import { Footer, Header } from "./Layout";
+//import "bootstrap/dist/js/bootstrap.min.css"
+
+
+
+function App() {
+  return <div className="text-success">
+    <Header />
+    <Footer />
+  </div>;
+}
+
+export default App;
---
+++ src/Container/Layout/Footer.tsx
@@ -0,0 +1,10 @@
+function Footer() {
+  return (
+    <div className="foter fixed-bottom text-center p-3 bg-dark text-white">
+      &copy; Made with <i className="bi bi-heart-fill"></i> love by
+      DotNetMastery Footer
+    </div>
+  );
+}
+
+export default Footer;
